b67e6674621eac4defebac2e582cb5e61cc2379c
Neil Hanlon <neil@rockylinux.org>
2023-05-15 23:18:43 -0400

    fixes for generation script

--- tools/generate-dockerhub.sh
+++ tools/generate-dockerhub.sh
@@ -9,8 +9,8 @@ major=${1:0:1}
 minor=${1:2:1}
 
 usage() {
-  printf "%s: RELEASE TYPE [DATE]\n\n" $0
-  log "$1"
+	printf "%s: RELEASE TYPE [DATE]\n\n" $0
+	log "$1"
 }
 
 # shellcheck disable=SC2046,1091,1090
@@ -21,27 +21,29 @@ name="Rocky-${version}.${date}-${type}"
 arches=(x86_64 aarch64)
 set -x
 if [[ $major -ge 9 ]]; then
-  arches=(${arches[@]} s390x ppc64le)
+	arches=(${arches[@]} s390x ppc64le)
 fi
 
-case $type in 
-  UBI|Minimal)
-    suffix="-${type,,}" ;;
-  *)
-    suffix='';;
+case $type in
+UBI | Minimal)
+	suffix="-${type,,}"
+	;;
+*)
+	suffix=''
+	;;
 esac
 
 declare -A shasums
 
 for a in "${arches[@]}"; do
-  pt="${name}-${a}"
-  if has-branch $pt; then
-    shasums[$a]="$(git rev-parse $pt)"
-  fi
+	pt="${name}-${a}"
+	if has-branch $pt; then
+		shasums[$a]="$(git rev-parse $pt)"
+	fi
 done
 
 cat <<EOF
-Tags: ${version}.${date}${suffix}, ${major}${suffix}, ${version}${suffix}
+Tags: ${version}.${date}${suffix}, ${version}${suffix}, ${major}${suffix}
 GitFetch: refs/heads/${name}-x86_64
 GitCommit: ${shasums[x86_64]}
 arm64v8-GitFetch: refs/heads/${name}-aarch64
@@ -52,4 +54,3 @@ ppc64le-GitFetch: refs/heads/${name}-ppc64le
 ppc64le-GitCommit: ${shasums[ppc64le]}
 Architectures: amd64, arm64v8, ppc64le, s390x
 EOF
-
